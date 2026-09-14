5d7d24fbd599403b284fe0ca692a5160d0197c38
Wadoud Ramanou <wadoudramanou5@gmail.com>
2026-09-14 19:33:30 +0100

    tt

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/alamou" element={<AdminProjets />} />
+       
       </Routes>
     </BrowserRouter>
   </StrictMode>,
